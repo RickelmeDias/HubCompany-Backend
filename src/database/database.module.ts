8a7ad32c6fe5f4d3fb61c537cbb314fb1b0e021b
Rickelme Dias <rickelmegadias@gmail.com>
2022-02-26 03:26:39 -0300

    Heroku database using SSL to connect

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,14 +9,18 @@ import { UserEntity } from 'src/user/entities/user.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
+      url: process.env.DB_URL,
       port: parseInt(process.env.DB_PORT) || 15432 || 5432,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       entities: [UserEntity, CompanyEntity, PlaceEntity],
       synchronize: true,
-      ssl: {
-        ca: process.env.SSL_CERT,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
     }),
   ],
